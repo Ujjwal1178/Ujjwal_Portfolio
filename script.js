fac2d205054b7dd31cfb6c218a299d959a1cd32a
Ujjwal <ujjawalthakur687@gmail.com>
2024-09-22 19:01:31 +0530

    FIrst Commit

---
+++ script.js
@@ -0,0 +1,87 @@
+const sideMenu = document.querySelector("#sideMenu");
+const navBar = document.querySelector("nav");
+const navLinks = document.querySelector("nav ul");
+const link1 = "https://github.com/Ujjwal1178/Netflix-clone.git";
+const link2 = "https://github.com/Ujjwal1178/wanderLust_project.git";
+const link3 = "https://github.com/Ujjwal1178/Text-Utils.git";
+const link4 = "https://github.com/Ujjwal1178/Netflix-clone.git";
+localStorage.theme = "light";
+function openMenu() {
+  sideMenu.style.transform = "translateX(-16rem)";
+}
+function closeMenu() {
+  sideMenu.style.transform = "translateX(16rem)";
+}
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navBar.classList.add(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm",
+      "dark:bg-darkTheme",
+      "dark:shadow-white/20"
+    );
+    navLinks.classList.remove(
+      "bg-white",
+      "shadow-sm",
+      "bg-opacity-50",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
+    );
+  } else {
+    navBar.classList.remove(
+      "bg-white",
+      "bg-opacity-50",
+      "backdrop-blur-lg",
+      "shadow-sm",
+      "dark:bg-darkTheme",
+      "dark:shadow-white/20"
+    );
+    navLinks.classList.add(
+      "bg-white",
+      "shadow-sm",
+      "bg-opacity-50",
+      "dark:border",
+      "dark:border-white/50",
+      "dark:bg-transparent"
+    );
+  }
+});
+
+// -------- light mode and dark mode -----------
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
+function toggleTheme() {
+  document.documentElement.classList.toggle("dark");
+
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.theme = "dark";
+  } else {
+    localStorage.theme = "light";
+  }
+}
+
+function onclickProject1() {
+  window.open(link1, "_blank");
+}
+function onclickProject2() {
+  window.open(link2, "_blank");
+}
+function onclickProject3() {
+  window.open(link3, "_blank");
+}
+function onclickProject4() {
+  window.open(link4, "_blank");
+}
